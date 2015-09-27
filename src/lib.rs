baf35aaa4ac26f0e1dcf5d73fea598020a8b6f6e
ttacon <ttacon@gmail.com>
2015-09-27 09:14:10 -0400

    push

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+extern crate libc;
+
+use libc::{c_int};
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct CvPoint {
+    pub x: c_int,
+    pub y: c_int,
+}
+
+#[link(name="opencv", kind="static")]
+extern {
+    fn cvPoint(x: c_int, y: c_int) -> CvPoint;
+}
+
 #[test]
 fn it_works() {
+    unsafe {
+        let p = cvPoint(4, 5);
+        println!("got: {:?}", p);
+    }
 }
